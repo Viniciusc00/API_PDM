dcbc1da5e3712a4970c108bc1c5b7e8004431dec
Alexandre Tsolakis <alexandre.tsolakis@cocobambu.com>
2023-05-06 13:28:53 -0300

    adicionadas chamadas para uso de rotas de menu

--- API/app.js
+++ API/app.js
@@ -1,11 +1,13 @@
-const express = require ('express');
+const express = require('express');
 const body_parser = require('body-parser');
-const userRouter = require ('./routers/user.router');
+const userRouter = require('./routers/user.router');
+const menuRouter = require('./routers/menu.router');
 
 const app = express();
 
 app.use(body_parser.json());
 
-app.use('/',userRouter);
+app.use('/', userRouter);
+app.use('/', menuRouter);
 
 module.exports = app;
