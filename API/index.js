c5dfc0c6a836b353659c09689f4c3044c5dd8bd2
Frederico <paixao77@yahoo.com.br>
2023-05-06 22:02:32 -0300

    bug fix

--- API/index.js
+++ API/index.js
@@ -1,11 +1,8 @@
 const app = require ('./app');
 const db = require ('./config/db');
 const UserModel = require ('./model/user.model');
-<<<<<<< Updated upstream
-
-=======
 const PratosModel = require ('./model/pratos.model');
->>>>>>> Stashed changes
+
 const port = 3000;
 
 app.get('/',(req,res)=>{
